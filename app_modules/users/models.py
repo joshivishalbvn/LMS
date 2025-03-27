c236236c7e4fe384121c02b3e6aff25e2a71ce4e
Vishal Joshi <103119523+joshivishalbvn@users.noreply.github.com>
2025-03-27 12:05:03 +0530

    Update models.py

--- app_modules/users/models.py
+++ app_modules/users/models.py
@@ -30,6 +30,29 @@ class CustomManager(models.Manager):
         Normalize the email address by lowercasing the domain part of it.
         """
         return normalize_email(email)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Creates and returns a superuser."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and returns a regular user."""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)  # Ensure user is active by default
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
         
 
 class User(AbstractUser):
